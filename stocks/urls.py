4028ff30d604666de96779a0c981137603477bcf
dnitecki <dominick.nitecki@outlook.com>
2024-11-27 20:47:48 -0500

    udate stock responses

---
+++ stocks/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from stocks import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+app_name = 'stocks'
+
+urlpatterns = [
+    path('stock', views.GetStocks.as_view() ),
+    path('stock/info', views.GetStockInfo.as_view()),
+    path('stock/news', views.GetStockNews.as_view()),
+]
+
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- stocks/views.py
+++ stocks/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+# from pandas_datareader import data as pdr
+import yfinance as yf
 
-# Create your views here.
+class GetStocks(APIView):
+    def get(self, request):
+        if request.method =='GET':
+            ticker = request.GET.get('ticker', "")
+            start = request.GET.get('startDate', "")
+            end = request.GET.get('endDate', "")
+            
+            if ticker =="":
+                return Response("Missing Required Parameters", status=status.HTTP_400_BAD_REQUEST)
+            elif start =="":
+                return Response("Missing Required Parameters", status=status.HTTP_400_BAD_REQUEST)
+            elif end =="":
+                return Response("Missing Required Parameters", status=status.HTTP_400_BAD_REQUEST)
+            stock='pdr.get_data_yahoo(ticker, start=start, end=end)'
+            return Response({'data':stock}, status=status.HTTP_200_OK)
+
+class GetStockInfo(APIView):
+    def get(self,request):
+        if request.method =='GET':
+            ticker = request.GET.get('ticker',"")
+            if ticker == "":
+                return Response("Missing Required Parameters", status=status.HTTP_400_BAD_REQUEST)
+            stock = yf.Ticker(ticker)
+            stockInfo = stock.info
+            return Response({'data':stockInfo}, status=status.HTTP_200_OK)
+
+class GetStockNews(APIView):
+    def get(self,request):
+        if request.method =="GET":
+            ticker = request.GET.get('ticker',"")
+            if ticker =="":
+                return Response("Missing Required Parameters", status=status.HTTP_400_BAD_REQUEST)
+            stock = yf.Ticker(ticker)
+            stockNews = stock.news
+            return Response({'data':stockNews}, status=status.HTTP_200_OK)
