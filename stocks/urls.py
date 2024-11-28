a47b839c8b04bcc6b5b9fc10a2f1d1ae993b38b9
dnitecki <dominick.nitecki@outlook.com>
2024-11-28 16:19:51 -0500

    clean up and add history

--- stocks/urls.py
+++ stocks/urls.py
@@ -5,9 +5,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 app_name = 'stocks'
 
 urlpatterns = [
-    path('stock', views.GetStocks.as_view() ),
     path('stock/info', views.GetStockInfo.as_view()),
     path('stock/news', views.GetStockNews.as_view()),
+    path('stock/history', views.GetStockHistory.as_view()),
 ]
 
 
--- stocks/views.py
+++ stocks/views.py
@@ -1,33 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-# from pandas_datareader import data as pdr
 import yfinance as yf
-
-class GetStocks(APIView):
-    def get(self, request):
-        if request.method =='GET':
-            ticker = request.GET.get('ticker', "")
-            start = request.GET.get('startDate', "")
-            end = request.GET.get('endDate', "")
-            
-            if ticker =="":
-                return Response("Missing Required Parameters", status=status.HTTP_400_BAD_REQUEST)
-            elif start =="":
-                return Response("Missing Required Parameters", status=status.HTTP_400_BAD_REQUEST)
-            elif end =="":
-                return Response("Missing Required Parameters", status=status.HTTP_400_BAD_REQUEST)
-            stock='pdr.get_data_yahoo(ticker, start=start, end=end)'
-            return Response({'data':stock}, status=status.HTTP_200_OK)
 
 class GetStockInfo(APIView):
     def get(self,request):
         if request.method =='GET':
             ticker = request.GET.get('ticker',"")
             if ticker == "":
-                return Response("Missing Required Parameters", status=status.HTTP_400_BAD_REQUEST)
-            stock = yf.Ticker(ticker)
-            stockInfo = stock.info
+                return Response("Missing Required Parameters: Ticker", status=status.HTTP_400_BAD_REQUEST)
+            stockInfo = yf.Ticker(ticker).info
             return Response({'data':stockInfo}, status=status.HTTP_200_OK)
 
 class GetStockNews(APIView):
@@ -35,7 +17,23 @@ class GetStockNews(APIView):
         if request.method =="GET":
             ticker = request.GET.get('ticker',"")
             if ticker =="":
-                return Response("Missing Required Parameters", status=status.HTTP_400_BAD_REQUEST)
-            stock = yf.Ticker(ticker)
-            stockNews = stock.news
+                return Response("Missing Required Parameters: Ticker", status=status.HTTP_400_BAD_REQUEST)
+            stockNews = yf.Ticker(ticker).news
             return Response({'data':stockNews}, status=status.HTTP_200_OK)
+        
+class GetStockHistory(APIView):
+    def get(self,request):
+        if request.method =="GET":
+            ticker = request.GET.get('ticker',"")
+            period = request.GET.get('period',"")
+            if ticker =="":
+                return Response("Missing Required Parameters: Ticker", status=status.HTTP_400_BAD_REQUEST)
+            elif period == "":
+                data = yf.Ticker(ticker)
+                stockHistory = data.history(period='1mo')
+                return Response({'data':stockHistory}, status=status.HTTP_200_OK)
+            elif period:
+                data = yf.Ticker(ticker)
+                #['1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max']
+                stockHistory = data.history(period='12mo')
+                return Response({'data':stockHistory}, status=status.HTTP_200_OK)
